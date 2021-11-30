d282fe614432139f658c69d8678d57a43c841011
ikamuliana <94824304+ikamuliana@users.noreply.github.com>
2021-11-30 18:45:16 +0700

    Update Startup.cs

--- sesi10/TodoAppNoJWT/Startup.cs
+++ sesi10/TodoAppNoJWT/Startup.cs
@@ -34,7 +34,7 @@ namespace TodoAppNoJWT
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "New_TodoApp", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApp", Version = "v1" });
             });
         }
 
